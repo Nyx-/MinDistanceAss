708071f6a759e882efbc8a1f64b1fba336b628dc
Nyx- <mnhunter@outlook.com>
2017-05-20 18:59:01 +1000

    Tried adding in the basic operations counter, and also printing to csv
    It doesn't work and I don't know why...

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+int counter1;
+int counter2;
+
 vector<int> generateArray(std::mt19937 mt, int length, int maxVal) {
     //init array
     std::vector<int> arr(length);
@@ -30,36 +33,41 @@ void printArray(std::vector<int> arr, int length) {
 }
 
 int MinDistance1(std::vector<int> A) {
-    double dmin;
+    double dmin1;
     int n = (sizeof(A)/sizeof(A[0]));
-    dmin = std::numeric_limits<double>::infinity();
+    dmin1 = std::numeric_limits<double>::infinity();
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if ((i != j) && (abs(A[i] - A[j]) < dmin)) { //Basic Operation
-                dmin = abs(A[i] - A[j]);
+            counter1 = counter1 + 1;
+            if ((i != j) && (abs(A[i] - A[j]) < dmin1)) { //Basic Operation
+                dmin1 = abs(A[i] - A[j]);
             }
         }
     }
-    return dmin;
+    cout << endl << "dmin1: " << dmin1 << endl;
+    cout << endl << "counter1: " << counter1 << endl;
+    return dmin1;
 }
 
 int MinDistance2(std::vector<int> A) {
-    double dmin;
+    double dmin2;
     int n = (sizeof(A)/sizeof(A[0]));
     int temp = 0;
 
-    dmin = std::numeric_limits<double>::infinity();
+    dmin2 = std::numeric_limits<double>::infinity();
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             temp = abs(A[i] - A[j]);
-            if (temp < dmin) { //Basic Operation
-                dmin = temp;
+            counter2 = counter2 + 1;
+            if (temp < dmin2) { //Basic Operation
+                dmin2 = temp;
             }
         }
     }
-    cout << endl << "dmin: " << dmin << endl << endl;
-    return dmin;
+    cout << endl << "dmin2: " << dmin2 << endl;
+    cout << endl << "counter2: " << counter2 << endl << endl;
+    return dmin2;
 }
 
 
@@ -71,23 +79,29 @@ int main()
     int GROWTH_RATE = 10; //GROWTH_RATE 10 = Vector Length 10 & 20 & 30 & ... & LENGTH_MAX
     int VALUE_MAX = 1000; //Array values will generate from 1 to VALUE_MAX
 
+    int result1 = 0;
+    int result2 = 0;
+
     //Prepare CSV
     ofstream myfile;
     myfile.open ("outputMain.csv");
-    myfile << "Length (n)" << "," << "Basic Operations" << "," << "Theoretical Average Efficiency" << endl;
+    myfile << "Length (n)" << "," << "MinDistance1" << "," << "Min1 BO" << "," << "MinDistance2" << "," << "Min2 BO" << endl;
 
     int currentLength = LENGTH_MIN;
 
     while (currentLength < LENGTH_MAX) {
         for (int i = 0; i < currentLength - 1; i++) {
+            counter1 = 0;
+            counter2 = 0;
             //generate a new seed based on time for each array
             auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
             std::mt19937 mt(seed);
 
             vector<int> a = generateArray(mt, currentLength, VALUE_MAX);
             printArray(a, currentLength);
-            MinDistance1(a);
-            MinDistance2(a);
+            result1 = MinDistance1(a);
+            result2 = MinDistance2(a);
+            myfile << currentLength << "," << result1 << "," << counter1 << "," << result2 << "," << counter2 << endl;
         }
 
         currentLength += GROWTH_RATE;
