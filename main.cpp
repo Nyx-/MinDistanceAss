db8f035283c784a12cb90049f2ce8435c9416212
Jesse Stanger <jesse.stanger@gmail.com>
2017-05-21 16:09:36 +1000

    Jks restore globals, Fixed counters & dmin

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+int counter1 = 0;
+int counter2 = 0;
+
 vector<int> generateArray(std::mt19937 mt, int length, int maxVal) {
     //init array
     std::vector<int> arr(length);
@@ -37,41 +40,41 @@ void printArray(std::vector<int> arr, int length) {
     }
 }
 
-int MinDistance1(std::vector<int> A, int counter) {
+int MinDistance1(std::vector<int> A, int length) {
     double dmin;
-    int n = (sizeof(A)/sizeof(A[0]));
     dmin = std::numeric_limits<double>::infinity();
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            counter = counter + 1;
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < length; j++) {
+            counter1 = counter1 + 1;
             if ((i != j) && (abs(A[i] - A[j]) < dmin)) { //Basic Operation
+//                counter1 = counter1 + 1;
                 dmin = abs(A[i] - A[j]);
             }
         }
     }
-    printValue("dmin1: ", dmin);
-    printValue("counter1: ", counter);
+    printValue("     dmin1: ", dmin);
+    printValue("  counter1: ", counter1);
     return dmin;
 }
 
-int MinDistance2(std::vector<int> A, int counter) {
+int MinDistance2(std::vector<int> A, int length) {
     double dmin;
-    int n = (sizeof(A)/sizeof(A[0]));
     int temp = 0;
     dmin = std::numeric_limits<double>::infinity();
 
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = 0; i < length - 1; i++) {
+        for (int j = i + 1; j < length; j++) {
             temp = abs(A[i] - A[j]);
-            counter = counter + 1;
+            counter2 = counter2 + 1;
             if (temp < dmin) { //Basic Operation
+//                counter2 = counter2 + 1;
                 dmin = temp;
             }
         }
     }
-    printValue("dmin2: ", dmin);
-    printValue("counter2: ", counter);
+    printValue("     dmin2: ", dmin);
+    printValue("  counter2: ", counter2);
     return dmin;
 }
 
@@ -79,33 +82,32 @@ int MinDistance2(std::vector<int> A, int counter) {
 int main()
 {
     //CONFIG
-    int LENGTH_MIN = 10; //Minimum vector length
-    int LENGTH_MAX = 50; //Maximum vector length
-    int GROWTH_RATE = 10; //GROWTH_RATE 10 = Vector Length 10 & 20 & 30 & ... & LENGTH_MAX
-    int VALUE_MAX = 1000; //Array values will generate from 1 to VALUE_MAX
-
-    int result1 = 0;
-    int result2 = 0;
+    int LENGTH_MIN = 1; //Minimum vector length
+    int LENGTH_MAX = 10; //Maximum vector length
+    int GROWTH_RATE = 1; //GROWTH_RATE 10 = Vector Length 10 & 20 & 30 & ... & LENGTH_MAX
+    int VALUE_MAX = 100000; //Array values will generate from 1 to VALUE_MAX
 
     //Prepare CSV
     ofstream myfile;
     myfile.open ("outputMain.csv");
-    myfile << "Length (n)" << "," << "MinDistance1" << "," << "Min1 BO" << "," << "MinDistance2" << "," << "Min2 BO" << endl;
+    myfile << "Length (n)" << "," << "dmin1" << "," << "Min1 BO" << "," << "dmin2" << "," << "Min2 BO" << endl;
+
+
+    int result1 = 0;
+    int result2 = 0;
 
     int currentLength = LENGTH_MIN;
 
     while (currentLength < LENGTH_MAX) {
         for (int i = 0; i < currentLength - 1; i++) {
-            int counter1 = 0;
-            int counter2 = 0;
             //generate a new seed based on time for each array
             auto seed = chrono::high_resolution_clock::now().time_since_epoch().count();
             std::mt19937 mt(seed);
 
             vector<int> a = generateArray(mt, currentLength, VALUE_MAX);
             printArray(a, currentLength);
-            result1 = MinDistance1(a, counter1);
-            result2 = MinDistance2(a, counter2);
+            result1 = MinDistance1(a, currentLength);
+            result2 = MinDistance2(a, currentLength);
 
             myfile << currentLength << "," << result1 << "," << counter1 << "," << result2 << "," << counter2 << endl;
         }
